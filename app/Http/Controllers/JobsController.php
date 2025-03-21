f3393d9cbdfbb29321d560a4d5fe3580c7d99289
DevJ1003 <devjoshi1384@gmail.com>
2025-03-21 23:21:43 +0530

    commit:

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -10,17 +10,57 @@ use Illuminate\Http\Request;
 class JobsController extends Controller
 {
     // This method is used to display jobs page
-    public function index()
+    public function index(Request $request)
     {
         $categories = Category::where('status', 1)->get();
         $jobNatures = JobNature::where('status', 1)->get();
 
-        $jobs = Job::where('status', 1)->orderBy('created_at', 'DESC')->paginate(9);
+        $jobs = Job::where('status', 1);
+
+        // Search using keyword
+        if (!empty($request->keyword)) {
+            $jobs = $jobs->where(function ($query) use ($request) {
+                $query->orWhere('title', 'like', '%' . $request->keyword . '%');
+                $query->orWhere('keywords', 'like', '%' . $request->keyword . '%');
+            });
+        }
+
+        // Search using location
+        if (!empty($request->location)) {
+            $jobs = $jobs->where('location', $request->location);
+        }
+
+        // Search using category
+        if (!empty($request->category)) {
+            $jobs = $jobs->where('category_id', $request->category);
+        }
+
+        $jobNatureArray = [];
+        // Search using jobNature
+        if (!empty($request->jobNature)) {
+            // 1,2,3
+            $jobNatureArray = explode(',', $request->jobNature);
+            $jobs = $jobs->whereIn('job_nature_id', $jobNatureArray);
+        }
+
+        // Search using experience
+        if (!empty($request->experience)) {
+            $jobs = $jobs->where('experience', $request->experience);
+        }
+
+        if ($request->sort == '0') {
+            $jobs = $jobs->orderBy('created_at', 'ASC');
+        } else {
+            $jobs = $jobs->orderBy('created_at', 'DESC');
+        }
+
+        $jobs = $jobs->paginate(9);
 
         return view('front.jobs', [
             'categories' => $categories,
             'jobNatures' => $jobNatures,
             'jobs' => $jobs,
+            'jobNatureArray' => $jobNatureArray
         ]);
     }
 }
