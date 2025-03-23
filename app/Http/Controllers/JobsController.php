fc2a1bdeb3b12e40a5c11ad316a2015c4dc612de
DevJ1003 <devjoshi1384@gmail.com>
2025-03-23 17:53:18 +0530

    commit

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobNotificationEmail;
 use App\Models\Category;
 use App\Models\Job;
 use App\Models\JobApplication;
 use App\Models\JobNature;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class JobsController extends Controller
 {
@@ -112,6 +115,15 @@ class JobsController extends Controller
         $application->employer_id = $employer_id;
         $application->applied_date = now();
         $application->save();
+
+        // Send notification email to employer
+        $employer = User::where('id', $employer_id)->first();
+        $mailData = [
+            'employer' => $employer,
+            'user' => Auth::user(),
+            'job' => $job
+        ];
+        Mail::to($employer->email)->send(new JobNotificationEmail($mailData));
 
         return redirect('jobs')->with('success', 'You have successfully applied!');
     }
